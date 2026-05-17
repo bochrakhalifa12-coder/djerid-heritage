ee9423ce6f9c0dc2a521614da41db4d5e94ec37a
bochrakhalifa12-coder <bochrakhalifa12@gmail.com>
2026-05-17 20:51:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const maCase = document.getElementById('welcome-msg');
+
+if (maCase) {
+    const heure = new Date().getHours();
+    if (heure < 18) {
+        maCase.innerText = "صباح الخير (Bonjour) ! Bienvenue à Tozeur.";
+    } else {
+        maCase.innerText = "مساء الخير (Bonsoir) ! Bienvenue à Tozeur.";
+    }
+}
+
+function montrerInfo(sujet) {
+    const zoneTexte = document.getElementById('texte-explicatif');
+    if (zoneTexte) {
+        if (sujet === 'oasis') {
+            zoneTexte.innerText = "🧺 La Vannerie : Un savoir-faire ancestral qui transforme les feuilles de palmier en objets d'art !";
+        } 
+        else if (sujet === 'brique') {
+            zoneTexte.innerText = "🧱 Les briques en relief protègent les murs de la chaleur de l'été.";
+        } 
+        else if (sujet === 'plat') {
+            zoneTexte.innerText = "🍲 Art Culinaire : Une cuisine oasienne riche en saveurs, marquée par l'utilisation d'épices locales et de dattes.";
+        }
+    }
+}
+
+let nombreDeLikes = 0;
+
+function ajouterLike() {
+    nombreDeLikes = nombreDeLikes + 1;
+    const caseLikes = document.getElementById('compteur-likes');
+    if (caseLikes) {
+        caseLikes.innerText = nombreDeLikes;
+    }
+}
